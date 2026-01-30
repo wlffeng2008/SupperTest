6ccab179ff1a3edfdd53bfac49dece0f193f499f
wlffeng2008 <wlffeng2008@163.com>
2026-01-30 19:04:26 +0800

    111111

---
+++ SupperTest/SupperTestDlg.cpp
@@ -0,0 +1,181 @@
+
+// SupperTestDlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "SupperTest.h"
+#include "SupperTestDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CSupperTestDlg dialog
+
+
+
+CSupperTestDlg::CSupperTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_SUPPERTEST_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CSupperTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CSupperTestDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CSupperTestDlg message handlers
+
+BOOL CSupperTestDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+	m_pPCMDataDlg = new CPCMDataDlg();
+	m_pPCMDataDlg->Create(this);
+	m_pPictureDlg = new CPictrueViewDlg();
+	m_pPictureDlg->Create(this);
+	SetDlgItemPos2(m_pPCMDataDlg, IDC_STATIC_FRAME);
+	SetDlgItemPos2(m_pPictureDlg, IDC_STATIC_FRAME);
+
+	m_pPCMDataDlg->Show();
+
+	SetSpecialID(-1, IDC_CHECK_ONTOP);
+
+	return TRUE; 
+}
+
+void CSupperTestDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CSupperTestDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CSupperTestDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+BOOL CSupperTestDlg::OnCommand(WPARAM wParam, LPARAM lParam)
+{
+	switch (wParam)
+	{
+		case IDC_CHECK_SWITCH:
+		{
+			BOOL bShow = IsDlgButtonChecked(IDC_CHECK_SWITCH);
+			m_pPCMDataDlg->Show(!bShow);
+			m_pPictureDlg->Show(bShow);
+		}
+	default:
+		break;
+	}
+	return CDialogEx::OnCommand(wParam, lParam);	
+}
